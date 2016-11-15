ee74347895c6e4033dd06a7a9c565bfe297118d0
zacobian <zliberis@gmail.com>
2016-11-15 19:17:19 +0200

    Update accent.cpp

--- accent.cpp
+++ accent.cpp
@@ -24,13 +24,14 @@ class Accentator {
 class Salonicator: public Accentator {
   public:
     Salonicator(string t) {text=t;}
-    void express_text();
+    void pronounce_text();
         
 };
  
-void Salonicator::express_text() {
+void Salonicator::pronounce_text() {
   	
    int length = this->text.size(); 
+  	
    int found[length]; 
    string key = "l";
    found[0]= this->text.find_first_of(key);
@@ -39,17 +40,17 @@ void Salonicator::express_text() {
    for (int i=0; i<length; i++)
    {
   	 
-    if (found[i]==string::npos){break;}
-   
-    if (text[found[i]+1] != 'l') 
-    {this->text.insert(found[i], change);}    
-     
-    found[i+1]=this->text.find_first_of(key,found[i]+2);
+	    if (found[i]==string::npos){break;}
+
+	    if (text[found[i]+1] != 'l') 
+	    {this->text.insert(found[i], change);}    
+
+	    found[i+1]=this->text.find_first_of(key,found[i]+2);
     
-   }
+    }
   	
-   cout << "A guy from Thessaloniki pronounce this text like this: ";
-   cout << this->text << endl;
+    cout << "A guy from Thessaloniki pronounce this text like this: ";
+    cout << this->text << endl;
 
 }
 
@@ -57,11 +58,11 @@ void Salonicator::express_text() {
 class Cyprator: public Accentator {
   public:
     Cyprator(string t) {text = t;}
-    void express_text();
+    void pronounce_text();
     
 };
   
-void Cyprator::express_text() {
+void Cyprator::pronounce_text() {
 	
    int length = this->text.size();        // change the text's length and fill it with ' ', so as if 
    this->text.resize(length+1, ' ');      // one of the letters o,i is in the and of the string text
@@ -69,9 +70,8 @@ void Cyprator::express_text() {
    
    int letterO[length], letterI[length]; 
    string key1 = "o ";
-   string key2 = "i ";
+   
    letterO[0] = this->text.find(key1);
-   letterI[0] = this->text.find(key2);
    string change = "n";
    
    for(int i=0; i<length; i++)
@@ -81,10 +81,13 @@ void Cyprator::express_text() {
     	letterO[i+1]=this->text.find(key1,letterO[i]+1);
    } 
    
+   string key2 = "i ";   
+   letterI[0] = this->text.find(key2);
+   
    for(int i=0; i<length; i++)
    { 
     	if(letterI[i]==string::npos) { break; }
-        this->text.insert(letterI[i]+1, change);  
+    	this->text.insert(letterI[i]+1, change);  
     	letterI[i+1]=this->text.find(key2,letterI[i]+1); 
    }
   	
@@ -93,16 +96,69 @@ void Cyprator::express_text() {
   
 }
 
+class Larisator: public Accentator {
+  public:
+    Larisator(string t) {text=t;}
+    void pronounce_text();
+        
+};
+ 
+void Larisator::pronounce_text() {
+  	
+   int length = this->text.size();        // change the text's length and fill it with ' ', so as if 
+   this->text.resize(length+1, ' ');      // one the sequence ou is in the and of the string text
+   length = this->text.size();            // the match with the key will be achieved
+     
+   int letterU[length], letterE[length]; 
+   string key1 = "ou ";
+   //string change1 = "";
+   letterU[0]= this->text.find(key1);
+   string key2 = "e";
+   letterE[0]= this->text.find_first_of(key2);
+   string change2 = "i";
+   
+   for (int i=0; i<length; i++)
+   {
+  	 
+	    if (letterU[i]==string::npos){break;}
+	    //cout<<letterU[i-1]<<endl;
+	    this->text.erase(letterU[i], 2);   
+	    letterU[i+1]=this->text.find(key1,letterU[i]);
+    
+    }
+   
+    for (int i=0; i<length; i++)
+    {
+  	 
+	    if (letterE[i]==string::npos){break;}
+	    //cout<<letterE[i-1]<<endl;
+	    this->text.insert(letterE[i], change2);   
+	    letterE[i+1]=this->text.find_first_of(key2,letterE[i]+2);
+
+    }
+  	
+    cout << "A guy from Larisa pronounce this text like this: ";
+    cout << this->text << endl;
+
+}
+
 int main () {
-  string text;  
+  string *text;
+  text = new string;  
+
   cout << "Please write a text of your choice\n";
-  getline(cin,text);
-  cout << "The text you enter is: " << text << endl;
+  getline(cin,*text);
+  cout << "The text you enter is: " << *text << endl;
        
-  Salonicator Chatz(text);
-  Cyprator Petrakis(text);
-  Chatz.express_text();
-  Petrakis.express_text();
+  Salonicator Chatz(*text);
+  Cyprator Papandreou(*text);
+  Larisator Mousmoulos(*text);
+  
+  Chatz.pronounce_text();
+  Papandreou.pronounce_text();
+  Mousmoulos.pronounce_text();
+  
+  delete text;
    
   return 0;
 }
